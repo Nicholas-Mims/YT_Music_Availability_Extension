b724fb4dbf46dc5a9c886f9b50a5fe913b962816
Nicholas Mims <nicholaslmims@gmail.com>
2023-05-09 14:50:37 -0400

    Beginning of 5/9/23

--- scripts/background.js
+++ scripts/background.js
@@ -6,7 +6,8 @@ function makeHttpRequest(videoid) {
 }
 
 function editHTML(available) {
-    video_title_element = document.querySelector("h1, .ytd-watch-metadata")
+    console.log("START!");
+    video_title_element = document.querySelector("h1 > yt_formatted-string, .ytd-watch-metadata");
     availability_text = document.createElement("p");
     if(available){
         availability_text.textContent = 'Video Is Available: ✔️';
@@ -15,9 +16,10 @@ function editHTML(available) {
         availability_text.textContent = 'Video Is Unavailable: ❌';
         video_title_element.insertAdjacentElement("afterend", availability_text);
     }
+    console.log("done");
 }
 
-function checkVideoAvailable(tabId, changeInfo, tabInfo) {
+function checkVideoAvailable(tab) {
     //availability = false;
     // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     //     // use `url` here inside the callback because it's asynchronous!
@@ -25,13 +27,23 @@ function checkVideoAvailable(tabId, changeInfo, tabInfo) {
     //     console.log(url);
         
     // });
-    //editHTML(true);
-    if(changeInfo.url !== undefined){
+    if(tab.url !== undefined){
         console.log("Url not undefined");
-        if(changeInfo.url.match(urlRegex) != null) {
+        if(tab.url.match(urlRegex) != null) {
             console.log("Youtube video found!");
+            console.log("id: ${tab.id}");
+            //Edit YouTube HTML
+            chrome.scripting.executeScript({
+                target : {tabId : tab.id},
+                func : editHTML,
+                args : [true],
+            })
         }
     }
 }
 
-chrome.tabs.onUpdated.addListener(checkVideoAvailable); 
+//chrome.tabs.onUpdated.addListener(checkVideoAvailable); 
+chrome.action.onClicked.addListener(async (tab) => {
+    console.log(tab.id);
+    checkVideoAvailable(tab);
+});
