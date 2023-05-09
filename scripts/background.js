bbb5088f266f2904e8ec233c73cb5b460f66b80e
Nicholas Mims <nicholaslmims@gmail.com>
2023-05-09 17:57:18 -0400

    end of day 5/9/23

--- scripts/background.js
+++ scripts/background.js
@@ -1,11 +1,28 @@
 
 const urlRegex = /https:\/\/www.youtube.com\/watch\?v=[a-zA-Z0-9]*/;
+const request_url = "https://music.youtube.com/youtubei/v1/player?key=AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30";
 
-function makeHttpRequest(videoid) {
-
+function checkJson(response){
+    console.log("checking JSON");
+    //videojson = JSON.parse(response);
+    //console.log(videojson.playabilityStatus.status);
 }
 
-function editHTML(available) {
+async function makeFetchRequest(video_id) {
+    console.log("making request: " + video_id);
+    const response = await fetch(request_url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "referer": "https://music.youtube.com/watch?v=" + video_id
+        },
+        body: {"videoId":video_id, "context":{"client":{"clientName":"WEB_REMIX", "clientVersion":"1.20230424.01.00"}}},
+    });
+
+    return response.json();
+}
+
+function editHtml(available) {
     console.log("START!");
     video_title_element = document.querySelector("h1 > yt_formatted-string, .ytd-watch-metadata");
     availability_text = document.createElement("p");
@@ -20,7 +37,7 @@ function editHTML(available) {
 }
 
 function checkVideoAvailable(tab) {
-    //availability = false;
+    availability = false;
     // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     //     // use `url` here inside the callback because it's asynchronous!
     //     let url = tabs[0].url;
@@ -28,22 +45,31 @@ function checkVideoAvailable(tab) {
         
     // });
     if(tab.url !== undefined){
-        console.log("Url not undefined");
         if(tab.url.match(urlRegex) != null) {
             console.log("Youtube video found!");
-            console.log("id: ${tab.id}");
-            //Edit YouTube HTML
-            chrome.scripting.executeScript({
-                target : {tabId : tab.id},
-                func : editHTML,
-                args : [true],
-            })
+
+            // //Edit YouTube HTML
+            // chrome.scripting.executeScript({
+            //     target : {tabId : tab.id},
+            //     func : editHTML,
+            //     args : [true],
+            // })
+
+            //Make Fetch Request
+            //Example url: https://www.youtube.com/watch?v=dQw4w9WgXcQ
+            id_start_index = tab.url.search(/\/watch\?v=/i);
+            video_id = tab.url.substring(id_start_index + 9, id_start_index + 20);
+            response = makeFetchRequest(video_id);
+            console.log(response);
+
+            //Check JSON For Availability
+            //availability = checkJson(response);
         }
     }
 }
 
 //chrome.tabs.onUpdated.addListener(checkVideoAvailable); 
 chrome.action.onClicked.addListener(async (tab) => {
-    console.log(tab.id);
     checkVideoAvailable(tab);
+
 });
